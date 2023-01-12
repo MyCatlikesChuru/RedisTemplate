3f90f2b788a4b555ef895b2da25fcd33dc35ffb0
Lee JaeHyeok <dhfif718@gmail.com>
2023-01-12 16:42:39 +0900

    [feat] Redis Hash Key 관련 추가

--- src/main/java/com/spring/redis/common/RedisDao.java
+++ src/main/java/com/spring/redis/common/RedisDao.java
@@ -1,36 +1,55 @@
 package com.spring.redis.common;
 
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.Collection;
+import java.util.Map;
 
 @Component
 public class RedisDao {
 
-    private final RedisTemplate<String, String> redisTemplate;
+    private final RedisTemplate<String, Object> redisTemplate;
 
-    public RedisDao(RedisTemplate<String, String> redisTemplate) {
+    public RedisDao(RedisTemplate<String, Object> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
     public void setValues(String key, String data) {
-        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
         values.set(key, data);
     }
 
     public void setValues(String key, String data, Duration duration) {
-        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
         values.set(key, data, duration);
     }
 
-    public String getValues(String key) {
-        ValueOperations<String, String> values = redisTemplate.opsForValue();
+    public Object getValues(String key) {
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
         return values.get(key);
     }
 
     public void deleteValues(String key) {
         redisTemplate.delete(key);
     }
+
+    public void setHashOps(String key, Map<String,String> data) {
+        HashOperations<String, Object, Object> values = redisTemplate.opsForHash();
+        values.putAll(key, data);
+    }
+
+    public String getHashOps(String key, String hashKey) {
+        HashOperations<String, Object, Object> values = redisTemplate.opsForHash();
+        return values.hasKey(key, hashKey) ? (String) redisTemplate.opsForHash().get(key, hashKey) : new String();
+    }
+
+    public void deleteHashOps(String key, String hashKey) {
+        HashOperations<String, Object, Object> values = redisTemplate.opsForHash();
+        values.delete(key, hashKey);
+    }
+
 }
--- src/main/java/com/spring/redis/conroller/RedisController.java
+++ src/main/java/com/spring/redis/conroller/RedisController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 @RestController
@@ -20,6 +24,9 @@ public class RedisController {
 
     private final RedisDao redisDao;
 
+    /*
+    * 일반 Key / Values CRUD
+    * */
     @GetMapping("/save")
     public ResponseEntity saveKey(@RequestParam String name){
 
@@ -29,9 +36,10 @@ public class RedisController {
 
         return new ResponseEntity(HttpStatus.CREATED);
     }
+
+
     @GetMapping("/save/time")
     public ResponseEntity saveKeyTime(@RequestParam String name){
-
 
         redisDao.setValues("name",name, Duration.ofMillis(10000)); // 1초후 소멸
         log.info("# 생성완료 ! key = name / value = {} [소멸 시간 = 10초후]",name);
@@ -42,7 +50,7 @@ public class RedisController {
     @GetMapping("/get")
     public ResponseEntity getKey(){
 
-        String values = redisDao.getValues("name"); // redis value 가져오기
+        String values = (String) redisDao.getValues("name"); // redis value 가져오기
         log.info("# 조회완료 ! key = name / value = {}", values);
 
         return new ResponseEntity(HttpStatus.CREATED);
@@ -52,6 +60,57 @@ public class RedisController {
     public ResponseEntity deleteKey(){
 
         redisDao.deleteValues("name"); // redis value 가져오기
+        log.info("# 삭제완료 !");
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    /*
+     * Hash Key / Values CRUD
+     * */
+
+    //
+    @GetMapping("/save/hashops")
+    public ResponseEntity setHashOps(){
+
+        Map<String, String> like = new HashMap<>();
+        like.put("like","10");
+        like.put("view","200");
+        like.put("token","tokenExample");
+
+        redisDao.setHashOps("dhfif718@naver.com",like);
+
+        Set<String> set = like.keySet();
+        Iterator<String> iterator = set.iterator();
+        while(iterator.hasNext()){
+            String next = iterator.next();
+            log.info("# 생성완료 ! key = dhfif718@naver.com / fields = [{} : {}]", next, like.get(next));
+        }
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get/hashops")
+    public ResponseEntity getHashOps(){
+
+        String values1 = redisDao.getHashOps("dhfif718@naver.com", "like");
+        String values2 = redisDao.getHashOps("dhfif718@naver.com", "view");
+        String values3 = redisDao.getHashOps("dhfif718@naver.com", "token");
+
+        log.info("# 생성완료 ! key = dhfif718@naver.com / fields = [like : {}]", values1);
+        log.info("# 생성완료 ! key = dhfif718@naver.com / fields = [view : {}]", values2);
+        log.info("# 생성완료 ! key = dhfif718@naver.com / fields = [token : {}]", values3);
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/delete/hashops")
+    public ResponseEntity deleteHashOps(){
+
+        redisDao.deleteHashOps("dhfif718@naver.com", "like"); // redis value 가져오기
+        redisDao.deleteHashOps("dhfif718@naver.com", "view"); // redis value 가져오기
+        redisDao.deleteHashOps("dhfif718@naver.com", "token"); // redis value 가져오기
+
         log.info("# 삭제완료 !");
 
         return new ResponseEntity(HttpStatus.CREATED);
